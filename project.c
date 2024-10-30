bd06b8f9beda51004923d04a400a8ffc7d9c705c
brj01 <167606460+brj01@users.noreply.github.com>
2024-10-30 18:11:01 +0300

    Update project.c

--- project.c
+++ project.c
@@ -161,61 +161,79 @@ bool smoke(Player* currentPlayer, const char *coordinates) {
     }
 }
 
-bool torpedo(Player* currentPlayer, bool array[10][10], bool hit[10][10]) {
-    int rowOrCol;
-    printf("Do you want a row (0) or column (1)? ");
-    scanf("%d", &rowOrCol);
+bool artillery(bool sankShipLastTurn, bool array [10][10]) {
+    if (!sankShipLastTurn) {
+        printf("you cant currently use this function\n");
+        return false; }
+    
+    char coordinate[3];
+
+    printf("enter the top left coordinate of the 2x2 area: ");
+    scanf("%s ", *coordinate);
+
+    int row = atoi(coordinate + 1) - 1;
+    int col = coordinate[0] - 'A';
+
+    
+    bool b1 = fire(array[row][col], array);
+
+    if(row + 1 < 10 && col + 1 < 10) {
+        bool b2 = fire(array[row + 1][col], array);
+        bool b3 = fire(array[row][col + 1], array);
+        bool b4 = fire(array[row + 1][col + 1]); 
+        
+        if (b1 | b2 | b3 | b4) {
+            return true; }
+
+    else return false; }    
+}
+
+
+bool torpedo(bool sankShipLastTurn, bool array [10][10], bool hit [10][10]) {
+    if(!sankShipLastTurn) {
+        printf("you havent unlocked this feature yet");
+        return false; }
+    
+    int rowOrCol; //0 for row, 1 for col
+    bool chosen[10]; //row or column chosen
+    printf("do you want a row or column? ");
+    scanf("%d\n");
 
     if (rowOrCol == 0) {
         int row;
-        printf("What row do you want to torpedo? ");
-        scanf("%d", &row);
+        printf("what row do you want to torpedo? ");
+        scanf("%d\n", row);
 
-        if (row >= 0 && row < 10) {
-            for (int j = 0; j < 10; j++) {
-                fire(array[row][j]);
-                hit[row][j] = true; 
-            }
-            return true;
-        } else {
-            printf("Row out of bounds\n");
+        if(row >=0 && row < 10) {
+            for(int j = 0; j < 10; j++) {
+                chosen[j] = fire(array[row][j]);
+                hit[row][j] = true; }
+
+            for (int i = 0; i < chosen; i++) {
+                if (chosen[i]) return true;
+                else continue; }
             return false;
         }
-    } else {
+        else printf("row out of bound and not on grid");
+        return false;
+    }
+    else {
         char column[2];
-        printf("What column do you want to torpedo? ");
-        scanf("%s", column);
+        printf("what column do you want to torpedo? ");
+        scanf("%s\n", *column);
 
         int col = column[0] - 'A';
 
-        if (col >= 0 && col < 10) {
-            for (int i = 0; i < 10; i++) {
-                fire(array[i][col]);
-                hit[i][col] = true; 
-            }
-            return true;
-        } else {
-            printf("Column out of bounds\n");
+        if(col >=0 && col < 10) {
+            for(int i = 0; i < 10; i++) {
+                chosen[i] = fire(array[i][col]); 
+                hit[i][col] = true; }
+
+            for (int i = 0; i < chosen; i++) {
+                if (chosen[i]) return true;
+                else continue; }
             return false;
         }
-    }
-}
-
-bool artillery(Player* currentPlayer, const char *coordinates) {
-    int row = atoi(coordinates + 1) - 1;
-    int col = coordinates[0] - 'A';
-
-    if (row + 1 < 10 && col + 1 < 10) {
-        fire(array, array[row][col]);
-        fire(array, array[row + 1][col]);
-        fire(array, array[row][col + 1]);
-        fire(array, array[row + 1][col + 1]);
-        return true;
-    } else {
-        printf("Coordinates are out of bounds\n");
-        return false; 
-    }
-}
 
 int main() {
     Player player1, player2;
